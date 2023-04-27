0caea3b1b3bc2ece633d2995ba61d13e482d3699
pragti-chauhan <pragtichauhan1541@gmail.com>
2023-04-27 12:53:06 +0530

    Changes in server.js

--- src/server.js
+++ src/server.js
@@ -27,14 +27,14 @@ cloudinary.config({
 });
 
 /*  route to set new value to interval  */
-app.get("/set_interval", (req, res) => {
+app.get("https://mr-proctor.onrender.com/set_interval", (req, res) => {
     interval = req.query.interval * 60 * 1000;
     console.log("interval setted to " + interval + " miliseconds");
     res.send({ success: true }).status(200).end();
 });
 
 /*  route for creating new User */
-app.post("/createUser", async (req, res) => {
+app.post("https://mr-proctor.onrender.com/createUser", async (req, res) => {
     try {
         const id = uniqueId.time();
 
@@ -61,7 +61,7 @@ app.post("/createUser", async (req, res) => {
 });
 
 /*  route for uploading user images to cloudinary and then storing url to mongoDB */
-app.post("/upload-image", (req, res) => {
+app.post("https://mr-proctor.onrender.com/upload-image", (req, res) => {
     const imageData = req.body.image;
     const userid = req.body.userid;
 
